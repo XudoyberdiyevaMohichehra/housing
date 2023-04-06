5512204d3e428d8e1edbb5133d56b5b31cd3b3e4
Mohichehra <xudoyberdiyevam25@gmail.com>
2023-04-06 14:56:41 +0500

    package

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import {Container} from './style'
 
-export const Home =()=>{
+export const Generic =()=>{
   return(
     <Container>
-      <h1>home</h1>
+      <h1>Generic</h1>
     </Container>
   )
 }
-export default Home
+export default Generic

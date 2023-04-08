53ef4878a246a729381645e80647025c23b08d52
Mohichehra <xudoyberdiyevam25@gmail.com>
2023-04-08 16:54:47 +0500

    HouseCard created

---
+++ src/components/HouseCard/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Container} from './style'
+
+
+export const HouseCard =()=>{
+  return(
+    <Container>
+      <h1>HouseCard</h1>
+    </Container>
+  )
+}
+export default HouseCard

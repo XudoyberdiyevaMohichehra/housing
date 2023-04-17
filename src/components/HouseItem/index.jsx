16bba0f1250cff6738e50b5c2f788f4dedcfca1b
Mohichehra <xudoyberdiyevam25@gmail.com>
2023-04-17 16:19:38 +0500

    Houseitem in progress

--- src/components/HouseItem/index.jsx
+++ src/components/HouseItem/index.jsx
@@ -1,16 +1,193 @@
-
-
-import React from "react";
-import { Container,} from "./style";
-import Button from "../Generics/Button";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import useRequest from '../../hooks/useRequest'
+import { Wrapper, Container, Details, Section, Content, Icons, Description } from "./style";
+const {REACT_APP_BASE_URL: url} = process.env;
 
 
 export const HouseItem = () => {
+  const [data, setData] = useState({});
+  const params = useParams();
+  const request = useRequest();
+
+  useEffect(() => {
+    fetch(
+      `http://ec2-3-140-188-131.us-east-2.compute.amazonaws.com:8081/api/v1/houses/id/${params?.id}`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res?.data);
+        window.scrollTo(0, 0);
+      });
+  }, [params?.id]);
+
   return (
     <>
-    <Container>
-        <Button>HouseItem</Button>
-    </Container>
+      <Wrapper>
+        <Container flex={3}>
+            {/* <h1>{data?.address}</h1> */}
+            <Section>
+              <Content style={{ alignItems: "flex-start" }}>
+                <div>
+                  <Content.Title large>{data.name}</Content.Title>
+                </div>
+                <div>
+                  <div className="info">
+                    {data?.city} {data?.address}
+                  </div>
+                </div>
+              </Content>
+              <Content flex>
+                <Icons.Share /> <Icons.Text>Share</Icons.Text>
+                <Icons.Like /> <Icons.Text>Save</Icons.Text>
+              </Content>
+            </Section>
+          <Section>
+            <Details>
+              <Icons.Bed />
+              <Details.Title>Bed {data?.houseDetails?.beds || 0}</Details.Title>
+
+              <Icons.Bath />
+              <Details.Title>
+                Bath {data?.houseDetails?.bath || 0}
+              </Details.Title>
+
+              <Icons.Garage />
+              <Details.Title>
+                Garage {data?.houseDetails?.garage || 0}
+              </Details.Title>
+
+              <Icons.Ruler />
+              <Details.Title>
+                Ruler {data?.houseDetails?.area || 0}kv
+              </Details.Title>
+            </Details>
+            <Content>
+              <Content flex>
+                <del>
+                  <Details.Title> ${data?.price || 0}</Details.Title>
+                </del>
+                <div className="title">${data?.salePrice || 0}</div>
+              </Content>
+              <div className="info" style={{ textAlign: "end" }}>
+                {data?.user?.firstname}
+              </div>
+            </Content>
+          </Section>
+          <Content.Title>Description</Content.Title>
+          <Description>{data?.description}</Description>
+          <Content.Title>Feature</Content.Title>
+          <Section>
+            <Container gap={25}>
+              <Content flex>
+                <Icons.Bed />
+                <Details.Title>
+                  Air Conditioning {data?.houseDetails?.beds || 0}
+                </Details.Title>
+              </Content>
+              <Content flex>
+                <Icons.Garage />
+                <Details.Title>
+                  Barbeque {data?.houseDetails?.beds || 0}
+                </Details.Title>
+              </Content>
+              <Content flex>
+                <Icons.Bed />
+                <Details.Title>
+                  Dryer {data?.houseDetails?.beds || 0}
+                </Details.Title>
+              </Content>
+              <Content flex>
+                <Icons.Ruler />
+                <Details.Title>
+                  Gym {data?.houseDetails?.beds || 0}
+                </Details.Title>
+              </Content>
+            </Container>
+            <Container gap={25}>
+              <Content flex>
+                <Icons.Bed />
+                <Details.Title>
+                  Air Conditioning {data?.houseDetails?.beds || 0}
+                </Details.Title>
+              </Content>
+              <Content flex>
+                <Icons.Garage />
+                <Details.Title>
+                  Barbeque {data?.houseDetails?.beds || 0}
+                </Details.Title>
+              </Content>
+              <Content flex>
+                <Icons.Bed />
+                <Details.Title>
+                  Dryer {data?.houseDetails?.beds || 0}
+                </Details.Title>
+              </Content>
+              <Content flex>
+                <Icons.Ruler />
+                <Details.Title>
+                  Gym {data?.houseDetails?.beds || 0}
+                </Details.Title>
+              </Content>
+            </Container>
+            <Container gap={25}>
+              <Content flex>
+                <Icons.Bed />
+                <Details.Title>
+                  Air Conditioning {data?.houseDetails?.beds || 0}
+                </Details.Title>
+              </Content>
+              <Content flex>
+                <Icons.Garage />
+                <Details.Title>
+                  Barbeque {data?.houseDetails?.beds || 0}
+                </Details.Title>
+              </Content>
+              <Content flex>
+                <Icons.Bed />
+                <Details.Title>
+                  Dryer {data?.houseDetails?.beds || 0}
+                </Details.Title>
+              </Content>
+              <Content flex>
+                <Icons.Ruler />
+                <Details.Title>
+                  Gym {data?.houseDetails?.beds || 0}
+                </Details.Title>
+              </Content>
+            </Container>
+            <Container gap={25}>
+              <Content flex>
+                <Icons.Bed />
+                <Details.Title>
+                  Air Conditioning {data?.houseDetails?.beds || 0}
+                </Details.Title>
+              </Content>
+              <Content flex>
+                <Icons.Garage />
+                <Details.Title>
+                  Barbeque {data?.houseDetails?.beds || 0}
+                </Details.Title>
+              </Content>
+              <Content flex>
+                <Icons.Bed />
+                <Details.Title>
+                  Dryer {data?.houseDetails?.beds || 0}
+                </Details.Title>
+              </Content>
+              <Content flex>
+                <Icons.Ruler />
+                <Details.Title>
+                  Gym {data?.houseDetails?.beds || 0}
+                </Details.Title>
+              </Content>
+            </Container>
+          </Section>
+          <Content.Title mt={48} mb>
+            Location
+          </Content.Title>
+        </Container>
+      </Wrapper>
     </>
   );
 };
--- src/components/HouseItem/style.js
+++ src/components/HouseItem/style.js
@@ -1,6 +1,134 @@
 import styled from "styled-components";
 
+import { ReactComponent as share } from "../../assets/icons/share.svg";
+import { ReactComponent as like } from "../../assets/icons/love.svg";
+
+import { ReactComponent as bed } from "../../assets/icons/bed.svg";
+import { ReactComponent as bath } from "../../assets/icons/bath.svg";
+import { ReactComponent as garage } from "../../assets/icons/car.svg";
+import { ReactComponent as ruler } from "../../assets/icons/ruler.svg";
+
+import { ReactComponent as resize } from "../../assets/icons/resize.svg";
+
+
 export const Container = styled.div`
-  width: 100%;
+  display: flex;
+  flex-direction: column;
+  flex: ${({ flex }) => flex};
+  gap: ${({ gap }) => `${gap}px`};
+  ~ .user {
+    background: #ffffff;
+    border: 1px solid #e6e9ec;
+    border-radius: 3px;
+    margin-left: 20px;
+    padding: 24px;
+
+    gap: 24px;
+  }
   
+`;
+export const Wrapper = styled.div`
+  padding: var(--padding);
+  max-width: var(--width);
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+  display: flex;
+  display: flex;
+  padding-top: 24px;
+`;
+
+export const Section = styled.div`
+  display:flex;
+  justify-content:space-between;
+`;
+export const Content = styled.div`
+  display:flex;
+  flex-direction:${({flex})=> !flex ? 'column' : 'row'};
+  align-items: ${({ flex }) => flex && "center"};
+  justify-content: ${({ flex }) => !flex && "center"};
+  `;
+Content.Title = styled.div`
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 600;
+  font-size: ${({ large }) => (large ? "24px" : "18px")};
+  line-height: 32px;
+  letter-spacing: -0.02em;
+  color: #0d263b;
+  margin-top: ${({ mt }) => `${mt}px`};
+  margin-bottom: ${({ mb }) => `${mb}px`};
+`;
+
+export const Details = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 32px 0;
+`;
+
+Details.Title = styled.div`
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  color: #696969;
+  margin: 0 24px 0 8px;
+`;
+
+export const Icons = styled.div``;
+
+Icons.Share = styled(share)`
+  display: flex;
+  padding: 8px;
+  background-color: #f6f8f9;
+  border-radius: 10px;
+  width: 35px;
+  height: 35px;
+  margin: 0 10px 0 26px;
+  cursor: pointer;
+  :active {
+    transform: scale(0.95);
+  }
+`;
+
+Icons.Like = styled(like)`
+  display: flex;
+  padding: 8px;
+  background-color: #f6f8f9;
+  border-radius: 10px;
+  width: 35px;
+  height: 35px;
+  margin: 0 10px 0 26px;
+  cursor: pointer;
+  :active {
+    transform: scale(0.95);
+  }
+`;
+
+Icons.Resize = styled(resize)``;
+Icons.Bed = styled(bed)``;
+Icons.Bath = styled(bath)``;
+Icons.Garage = styled(garage)``;
+Icons.Ruler = styled(ruler)``;
+
+Icons.Text = styled.div`
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  color: #696969;
+`;
+
+export const Description = styled.div`
+  margin-top: 16px;
+  background: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #ffffff 100%);
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  color: #696969;
+  margin-bottom: 48px;
 `;

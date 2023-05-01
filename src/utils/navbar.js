7c71092bb2aa60467c3dd4254e3641097b4fa512
Mohichehra <xudoyberdiyevam25@gmail.com>
2023-05-01 05:16:54 +0500

    utils

--- src/utils/navbar.js
+++ src/utils/navbar.js
@@ -69,6 +69,14 @@ export const navbar = [
         private: true,
         hidden: true,
     },
+    {
+        id: useId,
+        element: <AddNewHousePage />,
+        title: "Add New House",
+        path: "/myprofile/edithouse/:id",
+        private: true,
+        hidden: true,
+    },
     // { id: useId, element: <h1>Contacts</h1>, title: 'Contacts', path: '/contacts', private: false, hidden: false },
     { id: useId, element: <Register />, title: 'Sign In', path: '/register', private: false, hidden: true },
 ]

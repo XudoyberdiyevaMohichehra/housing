a1fffc510644791008ab250dea517830c81e8cb9
Mohichehra <xudoyberdiyevam25@gmail.com>
2023-05-02 21:20:58 +0500

    notfoundpage

---
+++ src/components/404page/index.jsx
@@ -0,0 +1,8 @@
+import notFound from "../../assets/img/robo404.jpg";
+
+export const Page =()=>{
+   return(
+    <img style={{width:'100%', height:'100vh'}} src={notFound} alt="" />
+   )
+}
+export default Page
--- src/root/index.jsx
+++ src/root/index.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom"
 import { navbar } from "../utils/navbar"
 import Navbar from "../components/navbar"
 import ToTopButton from "../components/ToTopButton";
-
+import Page from '../components/404page'
 
 
 const Root =()=>{
@@ -14,9 +14,9 @@ const Root =()=>{
                  navbar.map(({ path, element, id }) => { 
                         return <Route key={id} path={path} element={element} /> })
                 }
-            <Route path="*" element={<h1>404</h1>}/>
             <Route path="/" element={<Navigate to={'/home'}/>}/>
           </Route>
+                    <Route path="*" element={<Page/>}/>
          </Routes>
             <ToTopButton />
         </BrowserRouter>
